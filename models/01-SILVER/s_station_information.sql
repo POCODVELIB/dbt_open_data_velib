1c26da7c05fc861e4962948c946c865146cf8171
said.housseine@talan.com <said.housseine@talan.com>
2026-04-23 08:44:39 +0000

    fix error merge

--- models/01-SILVER/s_station_information.sql
+++ models/01-SILVER/s_station_information.sql
@@ -8,24 +8,33 @@ Created     : 2026-04-22
 */
 {{
     config(
-        materialized="incremental",
-        schema="SILVER",
-        unique_key="station_id",
-        incremental_strategy="merge",
+        materialized='incremental',
+        unique_key=['station_id']
     )
 }}
 
-select
-    parse_json(_raw):station_id::int as station_id,
-    parse_json(_raw):stationcode::varchar as station_code,
-    parse_json(_raw):name::varchar as name,
-    parse_json(_raw):lat::float as lat,
-    parse_json(_raw):lon::float as lon,
-    parse_json(_raw):capacity::int as capacity,
-    _loaded_at
+with raw as (
+    select
+        parse_json(_raw):station_id::int as station_id,
+        parse_json(_raw):stationcode::varchar as station_code,
+        parse_json(_raw):name::varchar as name,
+        parse_json(_raw):lat::float as lat,
+        parse_json(_raw):lon::float as lon,
+        parse_json(_raw):capacity::int as capacity,
+        _loaded_at
+    from ANALYTICS.RAW.STATION_INFORMATION
+    {% if is_incremental() %}
+        where _loaded_at > (select max(_loaded_at) from {{ this }})
+    {% endif %}
+),
 
-from {{ source("raw", "STATION_INFORMATION") }}
+deduped as (
+    select *
+    from raw
+    qualify row_number() over (
+        partition by station_id
+        order by _loaded_at desc
+    ) = 1
+)
 
-{% if is_incremental() %}
-    where _loaded_at > (select max(_loaded_at) from {{ this }})
-{% endif %}
+select * from deduped
